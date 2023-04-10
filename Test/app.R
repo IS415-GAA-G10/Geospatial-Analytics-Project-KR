af7a738b21251f6381d2ca2a5752ec6cf5ebf74e
junhao2309 <teojunhao2309@gmail.com>
2023-04-10 19:17:40 +0800

    Commit app changes

--- Test/app.R
+++ Test/app.R
@@ -1,18 +1,16 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(sf)
 library(tmap)
 library(tidyverse)
 library(readr)
 library(shinythemes)
+library(leaflet)
+library(shinycssloaders)
+library(rgdal)
+library(spNetwork)
+library(spatstat)
+library(raster)
+library(maptools)
 options(shiny.maxRequestSize = 30*1024^2)
 
 ui <- navbarPage("Hospital Playlist",
@@ -70,25 +68,300 @@ ui <- navbarPage("Hospital Playlist",
                                     strong("Co-Location Analysis"),
                                     tags$ul(
                                       tags$li("Visualisation of Local Co-Location Points"),
-                                      tags$li("Co-Location Statistical Interpretation")))),
-                                
-                                
+                                      tags$li("Co-Location Statistical Interpretation")))
+                                  )
+                                )
                               )
                             )
-                          )
+                          ),
+                 tabPanel("Data Import", fluid = TRUE, icon=icon("database"),
+                          sidebarLayout(position = 'left',
+                                        sidebarPanel(fluid = TRUE, width = 3,
+                                                     tags$strong("RDS Data Import (Healthcare):"),
+                                                     tags$br(),
+                                                     tags$i("Upload two RDS files containing point data"),
+                                                     tags$hr(),
+                                                     fileInput("healthcare", 
+                                                               "Upload rds file of healthcare points",
+                                                               accept = c(".rds")),
+                                                     fileInput("other_variable", 
+                                                               "Upload rds file of another variable",
+                                                               accept = c(".rds")),
+                                                     fileInput("network",
+                                                               "Upload rds file of transport networks",
+                                                               accept = c(".rds")),
+                                                     fileInput("studyarea",
+                                                               "Upload rds file of study area shapefile",
+                                                               accept = c(".rds")),
+                                                     actionButton("submit", "Submit"),
+                                                     uiOutput("district_selector")
+                                                     ),
+                                        mainPanel(width = 9,
+                                                  withSpinner(tmapOutput("point_map")),
+                                                  
+                          )),
                  ),
-                 tabPanel("Conventional Spatial Point Pattern Analysis",
-                          fluidPage(
+                 tabPanel("Conventional Spatial Point Pattern Analysis", fluid = TRUE,
+                          
+                                        ),
+                 tabPanel("Co-Location Analysis",fluid = TRUE,
+                          sidebarLayout(position = 'left',
+                                        sidebarPanel(fluid = TRUE, width = 3,
+                                                     ),
+                                        mainPanel(width = 9)
+                                        )
+                          ),
+                 tabPanel("Network Constrained Spatial Point Analysis", fluid = TRUE,
+                            sidebarLayout(position = 'left',
+                                          sidebarPanel(fluid = TRUE, width = 3,
+                                                       conditionalPanel(
+                                                         'input.NetKDE === "Network Kernal Density Estimate Visualisation"',
+                                                         tags$strong("Network Kernel Density Estimation Variable Inputs"),
+                                                         selectInput(inputId = "kernel",
+                                                                     label = "Choose the Kernel to be used:",
+                                                                     choices = c("Quartic" = "quartic",
+                                                                                 "Triangle" = "triangle",
+                                                                                 "Tricube" = "tricube",
+                                                                                 "Cosine" = "cosine",
+                                                                                 "Triweight" = "triweight",
+                                                                                 "Epanechnikov" = "epanechnikov",
+                                                                                 "Uniform" = "uniform"),
+                                                                     selected = "quartic"),
+                                                         selectInput(inputId = "method",
+                                                                     label = "Select the Method to be used:",
+                                                                     choices = c("Simple" = "simple",
+                                                                                 "Discontinuous" = "discontinuous", 
+                                                                                 "Continuous" = "continuous"),
+                                                                     selected = "simple"),
+                                                         tags$strong("Visualisation Customisation"),
+                                                         selectInput("palette", "Select color palette:",
+                                                                     choices = c("Reds", "Blues", "magma", "inferno", "cividis")),
+                                                         selectInput("colour", "Select healthcare points color:",
+                                                                     choices = c("red", "green", "blue", "yellow", "purple", "lightblue")),
+                                                         sliderInput("dot_size", "Select dot size:",
+                                                                     min = 0.01, max = 0.1, value = 0.01, step = 0.01),
+                                                         actionButton("NetKDE_Run", "Run Analysis"),
+          
+                                                       ),
+                                                       conditionalPanel(
+                                                         'input.NetKDE === "Network Constrained K-Function Analysis"',
+                                                         numericInput(inputId = "N_SIM",
+                                                                      label = "Number of Simulations: (key in a number between 0 to 1000)",
+                                                                      min = 0,
+                                                                      max = 1000,
+                                                                      step = 1,
+                                                                      value = 99),
+                                                         numericInput(inputId = "conf",
+                                                                      label ="Confidence Interval: (Key in either, 0.01, 0.05, 0.10)",
+                                                                      min = 0.01,
+                                                                      max = 0.10,
+                                                                      value = 0.05),
+                                                         sliderInput(inputId = "end_distance",
+                                                                     label = "Select end distance",
+                                                                     min = 50, 
+                                                                     max = 10000,
+                                                                     value = 5000,
+                                                                     step = 50),
+                                                         actionButton("NetKDE_Kfunc_run", "Run Analysis")
+                                                         
+                                                         
+                                                         
+                                                       ),
+                                                       conditionalPanel(
+                                                         'input.NetKDE === "Network Constrained K-Cross Function Analysis"',
+                                                         numericInput(inputId = "N_SIM",
+                                                                      label = "Number of Simulations: (key in a number between 0 to 1000)",
+                                                                      min = 0,
+                                                                      max = 1000,
+                                                                      step = 1,
+                                                                      value = 99),
+                                                         numericInput(inputId = "conf",
+                                                                      label ="Confidence Interval: (Key in either, 0.01, 0.05, 0.10)",
+                                                                      min = 0.01,
+                                                                      max = 0.10,
+                                                                      value = 0.05),
+                                                         sliderInput(inputId = "end_distance",
+                                                                     label = "Select end distance",
+                                                                     min = 50, 
+                                                                     max = 10000,
+                                                                     value = 5000,
+                                                                     step = 50),
+                                                         actionButton("NetKDE_Kcross_run", "Run Analysis")
+                                                         
+                                                       )
+                                          ),
+                                          mainPanel(width = 9,
+                                                    tabsetPanel(
+                                                      id = "NetKDE",
+                                                      tabPanel("Network Kernal Density Estimate Visualisation",
+                                                               tmapOutput("NetKDE_V")
+                                                      ),
+                                                      tabPanel("Network Constrained K-Function Analysis",
+                                                               plotOutput("NetKDE_Kfunction")
+                                                      ),
+                                                      tabPanel("Network Constrained K-Cross Function Analysis",
+                                                               plotOutput("NetKDE_Kcross")
+                                                      )
+                                                    )
+                                                    )
                             
-                          )),
-                 tabPanel("Network Constrained Spatial Point Analysis",
-                          fluidPage(
-                            
-                          )),
-                 tabPanel("Co-Location Analysis")
+                                          )
+                          
+                          )
+                 )
                 
-)
 server <- function(input, output) {
+  check_file_extension <- function(filepath, ext) {
+    tools::file_ext(filepath) %in% ext
+  }
+  point1 <- reactiveVal(NULL)
+  point2 <- reactiveVal(NULL)
+  line1 <- reactiveVal(NULL)
+  studyarea <- reactiveVal(NULL)
+  
+  filtered_data <- reactiveVal(NULL)
+  
+  observeEvent(input$submit, {
+    shinyjs::show("loading_spinner")
+    # Check if a file is uploaded
+    req(c(input$healthcare,input$other_variable, input$network, input$studyarea))
+    
+    if (check_file_extension(input$healthcare$name, "rds") && 
+        check_file_extension(input$other_variable$name, "rds") && 
+        check_file_extension(input$network$name, "rds") &&
+        check_file_extension(input$studyarea$name, "rds")) {
+      # Read the .rds file into an object called healthcare, other_variable and network
+      point1(read_rds(input$healthcare$datapath))
+      point2(read_rds(input$other_variable$datapath))
+      line1(read_rds(input$network$datapath))
+      studyarea(read_rds(input$studyarea$datapath))
+      
+      Healthcare_filtered <- reactiveVal(NULL)
+      Variable_filtered <- reactiveVal(NULL)
+      Network_filtered <- reactiveVal(NULL)
+      Studyarea_filtered <- reactiveVal(NULL)
+      output$district_selector <- renderUI({
+        req(studyarea())
+        selectInput("selected_district", "Select Study Area:",
+                    choices = unique(studyarea()$name_en))
+      })
+      output$point_map <- renderTmap({
+        req(point1(), point2(), line1(), studyarea(), input$selected_district)
+        
+        selected_studyarea <- studyarea()[studyarea()$name_en == input$selected_district, ]
+        Healthcare <- point1()[point1()$name_en == input$selected_district, ]
+        Variable <- point2()[point2()$name_en == input$selected_district, ]
+        Network <- line1()[line1()$name_en == input$selected_district, ]
+        
+        Healthcare_filtered(Healthcare)
+        Variable_filtered(Variable)
+        Network_filtered(Network)
+        Studyarea_filtered(selected_studyarea)
+        
+        tm_shape(Network) +
+          tm_lines(col = "yellow", lwd = 1) +
+          tm_shape(Healthcare) +
+          tm_dots(col = "lightblue", size = 0.01, border.col = "black") +
+          tm_shape(Variable) +
+          tm_dots(col = "orange", size = 0.01, border.col = "black") +
+          
+          tm_layout(title = "Filtered Study Area and Intersections")
+      })
+      
+      # NetKDE Section
+          # NetKDE Visualisation
+      observeEvent(input$NetKDE_Run, {
+        output$NetKDE_V <- renderTmap({
+          req(Healthcare_filtered(), Variable_filtered(), Network_filtered(), Studyarea_filtered())
+          lixels <- lixelize_lines(Network_filtered(), 700, mindist = 350)
+          H <- Healthcare_filtered()
+          N <- Network_filtered()
+          samples <- lines_center(lixels)
+          densities <- nkde(N, 
+                            events = H,
+                            w = rep(1,nrow(H)),
+                            samples = samples,
+                            kernel_name = input$kernel,
+                            bw = 300, 
+                            div= "bw", 
+                            adaptive = FALSE,
+                            method = input$method, 
+                            digits = 1, 
+                            tol = 1,
+                            grid_shape = c(1,1), 
+                            max_depth = 8,
+                            agg = 5, #we aggregate events within a 5m radius (faster calculation)
+                            sparse = TRUE,
+                            verbose = FALSE)
+          samples$density <- densities*1000
+          lixels$density <- densities*1000
+          tm_shape(lixels)+
+            tm_lines(col="density",
+                     palette = input$palette) +
+            tm_shape(Healthcare_filtered()) +
+            tm_dots(col = input$colour,
+                    size = input$dot_size)
+        })
+      })
+          #NetKDE K Function
+      observeEvent(input$NetKDE_Kfunc_run, {
+        output$NetKDE_Kfunction <- renderPlot({
+          req(Healthcare_filtered(), Variable_filtered(), Network_filtered(), Studyarea_filtered())
+          H <- Healthcare_filtered()
+          N <- Network_filtered()
+          kfun_hospital <- kfunctions(N, 
+                                      H,
+                                      start = 0, 
+                                      end = input$end_distance, 
+                                      step = 50, 
+                                      width = 50, 
+                                      nsim = input$N_SIM, 
+                                      resolution = 50,
+                                      verbose = FALSE,
+                                      digits = 10,
+                                      conf_int = input$conf)
+          kfun_hospital$plotk
+        })
+      })
+          # Net KDE Kcross
+      observeEvent(input$NetKDE_Kcross_run, {
+        output$NetKDE_Kcross <- renderPlot({
+          req(Healthcare_filtered(), Variable_filtered(), Network_filtered(), Studyarea_filtered())
+          H <- Healthcare_filtered()
+          V <- Variable_filtered()
+          N <- Network_filtered()
+          crossfun_hospital<- cross_kfunctions(N, 
+                                               V, 
+                                               H, 
+                                               start= 0, 
+                                               end = input$end_distance, 
+                                               step = 50, 
+                                               width = 50, 
+                                               nsim = input$N_SIM,
+                                               agg = 100,
+                                               conf_int = input$conf)
+          crossfun_hospital$plotk
+        })
+      })
+      # End of NetKDE section.
+      
+      
+    } else {
+      # Show an error message if the uploaded files have the wrong extension
+      output$point_map <- renderTmap({
+        req(FALSE) # Stop rendering
+        NULL
+      })
+      showModal(modalDialog(
+        title = "Error",
+        "Please upload RDS files.",
+        easyClose = TRUE
+      ))
+    }
+  })
+  
+    
+    
 }
 
 shinyApp(ui = ui, server = server)
